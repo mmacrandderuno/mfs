c3d22da2ecec9c2c27b23aa7a6384684ab4705fe
= <mmacrander@unomaha.edu>
2019-02-05 21:53:06 -0600

    Removing old directory.

--- venv/mfs/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Cust, Service, Product
-
-admin.site.register(Cust)
-admin.site.register(Service)
-admin.site.register(Product)
--- venv/mfs/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MfsConfig(AppConfig):
-    name = 'mfs'
